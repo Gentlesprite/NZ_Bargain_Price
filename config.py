d3b1a0f2a0a162073cb7c5b63ace635bc3a7f8d4
Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>
2025-06-08 00:52:18 +0800

    适配2025年6月活动。
    Signed-off-by: Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # Software:PyCharm
 # Time:2024/10/1 11:40
 # File:config.py
-url = 'https://comm.ams.game.qq.com/ams/ame/amesvr?sServiceType=nz&iActivityId=717236&sServiceDepartment=group_a&sSDID=77fe8b0816e296b66e976e6837e20649'
+url = 'https://comm.ams.game.qq.com/ams/ame/amesvr?sServiceType=nz&iActivityId=722431&sServiceDepartment=group_a&sSDID=fdca75195df3a6fc48ab3e4bdd7d8302'
 # 定义每天执行的时间点（小时:分钟）
 schedule_times = [
     '17:30',
@@ -13,13 +13,13 @@ schedule_times = [
 ]
 cookies = '填写抓取的cookie'
 post_data = {'sServiceType': 'nz',
-             'iActivityId': '717236',
+             'iActivityId': '722431',
              'sServiceDepartment': 'group_a',
-             'iFlowId': '1122930',
+             'iFlowId': '1129012',
              'g_tk': '1842395457',
-             'sMiloTag': 'AMS-nz-0507135328-sfbOI3-717236-1122930',
-             'e_code': '547452',
+             'sMiloTag': 'AMS-nz-0607235538-gMNRhb-722431-1129012',
+             'e_code': '547858',
              'g_code': '0',
-             'eas_url': 'https%3A%2F%2Fnz.qq.com%2Fcp%2Fa20250502wyxlb%2F%3Fe_code%3D547452',
-             'eas_refer': 'https%3A%2F%2Fnz.qq.com%2Fcp%2Fa20250502wyxlb%2F%3Fe_code%3D547452'}
+             'eas_url': 'https%3A%2F%2Fnz.qq.com%2Fcp%2Fa20250601cxhg%2F%3Fe_code%3D547858',
+             'eas_refer': 'https%3A%2F%2Fnz.qq.com%2Fcp%2Fa20250601cxhg%2F%3Fe_code%3D547858'}
 push_key = 'server酱推送'
